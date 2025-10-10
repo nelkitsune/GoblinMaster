15438bd15c97d57038691fa8451a4e51305a1d98
RamiroFerrari <110360879+nelkitsune@users.noreply.github.com>
2025-10-10 14:51:28 -0300

    iniciado Spring Initialir

---
+++ src/main/java/ar/utn/tup/goblinmaster/GoblinMasterApplication.java
@@ -0,0 +1,13 @@
+package ar.utn.tup.goblinmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoblinMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoblinMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/utn/tup/goblinmaster/GoblinMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.utn.tup.goblinmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoblinMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
